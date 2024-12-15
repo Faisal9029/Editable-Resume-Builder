b635c8fcea65aefc13f2f8d06c9c6f72c1387827
Faisal9029 <faisalsharif39@yahoo.com>
2024-12-15 21:43:29 +0500

    Your commit message

---
+++ script.ts
@@ -0,0 +1,58 @@
+// Getting Refrences to the form and display area
+const profilepictureinput = document.getElementById('profilepicture') as HTMLInputElement | null
+const form = document.getElementById('resume-form') as HTMLFormElement
+const resumeDisplayelement = document.getElementById('resume-display') as HTMLDivElement
+
+
+//Handle form Submission
+form.addEventListener('submit', (event: Event) => {
+    event.preventDefault(); //prevent page reload
+
+    //Collect input values
+    const name = (document.getElementById('name') as HTMLInputElement).value;
+    const fname = (document.getElementById('f/name') as HTMLInputElement).value;
+    const CNIC = (document.getElementById('cnic') as HTMLInputElement).value;
+    const email = (document.getElementById('email') as HTMLInputElement).value;
+    const phone = (document.getElementById('phone') as HTMLInputElement).value;
+    const education = (document.getElementById('education') as HTMLInputElement).value;
+    const experience = (document.getElementById('experience') as HTMLInputElement).value;
+    const skills = (document.getElementById('skills') as HTMLInputElement).value;
+
+    //Profile picture elements
+    const profilepicturefile = profilepictureinput?.files?.[0]
+    const profilepictureURL = profilepicturefile ? URL.createObjectURL(profilepicturefile) : '';
+
+    //Generate the resume details dynamically
+    const resumeHTML=`
+    <h2><b><center><u>Editable Resume</u></b></h2>
+    ${profilepictureURL ? `<img src="${profilepictureURL}" alt="Profile Picture" class="profilepicture">` : ''}
+    <h3>Personal Information</h3>
+    <p><b>Name: </b><span contenteditable="true">${name}</span></p> 
+    <p><b>F/Name: </b><span contenteditable="true">${fname}</span></p>
+    <p><b>CNIC: </b><span contenteditable="true">${CNIC}</span></p>
+    <p><b>Email: </b><span contenteditable="true">${email}</span></p>
+    <p><b>Contact No: </b><span contenteditable="true">${phone}</span></p>
+    
+    <h3>Education</h3>
+    <p contenteditable="true">${education}</p>
+
+    <h3>Experience</h3>
+    <p contenteditable="true">${experience}</p>
+
+    <h3>Skills</h3>
+    <p contenteditable="true">${skills}</p>
+    `;
+    
+
+    //Display the generated resume
+    if(resumeDisplayelement){
+        resumeDisplayelement.innerHTML = resumeHTML;
+    }else {
+        console.error('The resume display element is missing.')
+    }
+
+    
+});
+
+
+//Made Editable Resume by using span contenteditable="true" 
